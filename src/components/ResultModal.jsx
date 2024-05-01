1a16eecc8e421555c2ab088f6428be8f851b984e
jacquelyn-bradshaw <jacquelyn_bradshaw@live.co.uk>
2024-05-01 11:37:42 +0100

    UI improved in the modal when a player won

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -4,8 +4,8 @@ const ResultModal = forwardRef( function ResultModal ({ remainingTime, targetTim
   const dialog = useRef();
 
   const userLost = remainingTime <= 0
-
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -15,8 +15,9 @@ const ResultModal = forwardRef( function ResultModal ({ remainingTime, targetTim
     }
   });
 
-  return <dialog ref={dialog} className="result-modal" open>
+  return <dialog ref={dialog} className="result-modal" onClose={onReset} open>
     {userLost && <h2>You lost</h2>}
+    {!userLost && <h2>Your score: {score}</h2>}
     <p>The target time was <strong>{targetTime} seconds.</strong></p>
     <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
     <form method="dialog" onSubmit={onReset}>
